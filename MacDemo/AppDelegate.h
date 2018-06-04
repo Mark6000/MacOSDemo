5390283e49cab154ef633ea88005548704ff886b
“张国微” <zhangguowei@1rock.net>
2018-06-04 14:17:40 +0800

    Initial Commit

---
+++ MacDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacDemo
+//
+//  Created by catbook on 2018/6/4.
+//  Copyright © 2018年 catbook. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MacDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MacDemo
+//
+//  Created by catbook on 2018/6/4.
+//  Copyright © 2018年 catbook. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
